101c5692f6247a96d1ca5751461943d163ff8c09
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-18 19:13:19 +0900

    Update executable

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,43 +1,30 @@
 
 extern crate itertools;
-extern crate ndarray;
 extern crate ndarray_odeint;
-extern crate num_complex;
 extern crate spectral;
-extern crate fftw;
 
-use itertools::iterate;
-use num_complex::Complex64 as c64;
-use ndarray::*;
-use ndarray_odeint::prelude::*;
 use std::f64::consts::PI;
-use spectral::kse::*;
-use fftw::*;
+use itertools::iterate;
+use ndarray_odeint::prelude::*;
 
-fn init_data(n: usize, l: f64) -> RcArray1<c64> {
-    let mut pair = Pair::r2c_1d(n, FLAG::FFTW_ESTIMATE);
-    let k0 = 2.0 * PI / l;
-    for (i, val) in pair.field.iter_mut().enumerate() {
-        let x = l * i as f64 / n as f64;
-        *val = (k0 * x).sin();
-    }
-    pair.forward();
-    RcArray::from_iter(pair.coef.iter().cloned())
-}
+use spectral::kse::*;
+use spectral::coef_1d;
 
 fn main() {
     let n = 128;
-    let l = 12.0;
-    let dt = 1e-7;
+    let l = 32.0;
+    let k0 = 2.0 * PI / l;
+    let dt = 1e-4;
     let eom = KSE::new(n, l);
     let mut teo = semi_implicit::diag_rk4(eom, dt);
 
-    let x0 = init_data(n, l);
+    let x0 = coef_1d(n, l, |x| (3.0 * k0 * x).cos() + (2.0 * k0 * x).cos());
+    let n_coef = x0.len();
     let ts = iterate(x0, |y| teo.iterate(y.clone()));
-    let end_time = 1000;
+    let end_time = 10000;
 
     print!("time");
-    for i in 0..n {
+    for i in 0..n_coef {
         print!(",r{},c{}", i, i);
     }
     println!("");
